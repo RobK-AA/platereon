76ca6c049bfcfdcfa3ee57d1c96d126967ceae3c
Robert Kornblum <robkornblum44@gmail.com>
2020-07-27 17:54:10 -0400

    Prepare for initial Heroku push

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,27 +1,27 @@
-class Api::SessionsController < ApplicationController
+# class Api::SessionsController < ApplicationController
   
-  def create
-    @user = User.find_by_credentials(
-      params[:user][:username],
-      params[:user][:password]
-    )
+#   def create
+#     @user = User.find_by_credentials(
+#       params[:user][:username],
+#       params[:user][:password]
+#     )
 
-    if @user
-      login!(@user)
-      render 'api/users/show'
-    else
-      render json: ['Invalid username or password']
-    end
-  end
+#     if @user
+#       login!(@user)
+#       render 'api/users/show'
+#     else
+#       render json: ['Invalid username or password']
+#     end
+#   end
 
-  def destroy
-    @user = current_user
-    if @user
-      logout!
-      render 'api/users/show'
-    else
-      render json: ['Not logged in']
-    end
-  end
+#   def destroy
+#     @user = current_user
+#     if @user
+#       logout!
+#       render 'api/users/show'
+#     else
+#       render json: ['Not logged in']
+#     end
+#   end
 
-end
+# end
