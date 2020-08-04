9c4d6afecab4d8ba921cf070e6cacd8efc9a671e
Robert Kornblum <robkornblum44@gmail.com>
2020-08-04 10:52:17 -0400

    update communities

--- app/controllers/api/communities_controller.rb
+++ app/controllers/api/communities_controller.rb
@@ -18,17 +18,8 @@ class Api::CommunitiesController < ApplicationController
 
   def update
     @community = Community.find(params[:id])
-    @community.update(
-      name: community_params[:community][:name],
-      description: community_params[:community][:description],
-      bronze_perks: community_params[:community[:bronze_perks],
-      silver_perks: community_params[:community][:silver_perks],
-      gold_perks: community_params[:community][:gold_perks],
-      short_description: community_params[:community][:short_description],
-      plural: community_params[:community][:plural]
-    )
-
-    if @community.update
+    
+    if @community.update(community_params)
       render "api/communities/show"
     else
       render json: @community.errors.full_messages, status: 422
