b594aad315cee5646c43c15e4067c208528bc2e9
Robert Kornblum <robkornblum44@gmail.com>
2020-09-11 12:53:33 -0400

    create post api util

---
+++ frontend/actions/post_actions.js
@@ -0,0 +1,5 @@
+import * as PostApiUtil from '../util/post_api_util';
+
+export const RECEIVE_POSTS = 'RECEIVE_POSTS';
+export const RECEIVE_POST = 'RECEIVE_POST';
+export const REMOVE_POST = 'REMOVE_POST';
---
+++ frontend/util/post_api_util.jsx
@@ -0,0 +1,34 @@
+export const fetchPosts = (communityId) => {
+  return $.ajax({
+    url: `api/communities/${communityId}/posts`
+  })
+}
+
+export const fetchPost = (postId) => {
+  return $.ajax({
+    url: `api/posts/${postId}`
+  })
+}
+
+export const createPost = (post) => {
+  return $.ajax({
+    url: `api/posts`,
+    method: `POST`,
+    data: { post }
+  })
+}
+
+export const updatePost = (post) => {
+  return $.ajax({
+    url: `api/posts/${post.id}`,
+    method: `DELETE`,
+    data: { post }
+  })
+}
+
+export const deletePost = (postId) => {
+  return $.ajax({
+    url: `api/posts/${postId}`,
+    method: `DELETE`
+  })
+}
